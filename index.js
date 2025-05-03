83c47239c4d9bb4345e08aae52fcb0586958d9bd
Culip <mfhass1234@gmail.com>
2025-05-03 07:16:26 +0000

    9

--- index.js
+++ index.js
@@ -254,12 +254,3 @@ async function genAllReports() {
     await genReportLog(document.getElementById("reports"), key, url);
   }
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-  const navToggle = document.querySelector('.nav-toggle');
-  const navLinks = document.querySelector('.nav-links');
-  
-  navToggle?.addEventListener('click', () => {
-    navLinks?.classList.toggle('active');
-  });
-});
